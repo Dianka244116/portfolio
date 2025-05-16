d24dc6e91568e62a38b6bf6f4283ee29cf30141f
Dianka244116 <dianashevnakova@gmail.com>
2025-05-16 14:54:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 'use strict';
 window.onload = function () {
-    const bars = document.querySelectorAll("progress");
+    let bars = document.querySelectorAll("progress");
 
-    for (let index = 0; index < bars.length; i++) {
-        const bar = bars[index];
+    for (let i = 0; i < bars.length; i++) {
+        let bar = bars[i];
         let target = parseInt(bar.getAttribute("value"));
         bar.value = 0;
 
-        const current = 0;
+        let current = 0;
 
-        const timer = setInterval(function () {
+        let timer = setInterval(function () {
             if (current < target) {
                 current++;
                 bar.value = current;
             } else {
                 clearInterval(timer);
             }
-        }, 4);
+        }, 5);
     }
 };
+
+
 
 
 
